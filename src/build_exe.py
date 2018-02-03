f90c0caa58d7f05fe169ca5fad3e172477822dac
Javier Marcon <javiermarcon@gmail.com>
2018-02-03 00:02:29 -0300

    agregado de ini a build

--- src/build_exe.py
+++ src/build_exe.py
@@ -1,5 +1,6 @@
 #coding: cp1252
 
+import sys
 from cx_Freeze import setup, Executable
 
 from main import __version__
@@ -7,6 +8,8 @@ from main import __version__
 dest_base = "xmr-stak-monitor-{}".format(__version__)
 
 base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 executables = [Executable("main.py", 
 						base=base, 
@@ -16,12 +19,12 @@ packages = ['logging', 'getopt', 'sys', 'string',
 			'datetime', 'requests', 'schedule', 
 			'traceback', 'subprocess', 'sys',
 			'idna', 'eventlet', 'os', 'time',
-            'locale', 'dns'
+            'locale', 'certifi', 'urllib3', 'greenlet'
 							]
 options = {
     'build_exe': {
-
-        'packages':packages,
+        'packages': packages,
+        'include_files': ['config.ini'],
     },
 
 }
