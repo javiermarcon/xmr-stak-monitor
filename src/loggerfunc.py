66e5e897150dca5908969d8db89e80781f2ece66
Javier Marcon <javiermarcon@gmail.com>
2018-02-11 13:03:31 -0300

    FIX; el log grababa 2 veces cada linea porque se hacian 2 handlers al inicio.

--- src/loggerfunc.py
+++ src/loggerfunc.py
@@ -6,6 +6,11 @@ def loguear(options):
     logger = logging.getLogger('xmr-stak-monitor')
     logger.setLevel(options["level"])
     enc = locale.getpreferredencoding()
+    # si ya tengo un handler porque se lanzo con la config default, lo saco para
+    # evitar logging repetido
+    if logger.hasHandlers():
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
     handler = logging.handlers.RotatingFileHandler(
             options["filename"], maxBytes=int(options["max_bytes"]),
             backupCount=int(options["backup_count"]), encoding=enc)
